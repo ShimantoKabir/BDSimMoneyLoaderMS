5c3ce090ea78db416c3e832be30559751183f636
ShimantoKabir <maask5534@gmail.com>
2018-04-24 21:42:21 +0600

    invironment setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>BDSimMoneyLoaderMS</title>
+        <link rel="stylesheet" href="css/main.css" >
+        <script src="js/jquery.js" ></script>
+        <script src="js/main.js" ></script>
+    </head>
+    <body>
+        <p>If you click on the "Hide" button, I will disappear.</p>
+
+        <button id="hide">Hide</button>
+        <button id="show">Show</button>
+
+    <script>
+
+    </script>
+    </body>
+</html>
